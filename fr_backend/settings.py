2141ea5069d3f6f7e087c950a978892630d6954f
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-09-22 14:16:43 +0100

    Ajust PostgreSQL

--- fr_backend/settings.py
+++ fr_backend/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'fr_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'af_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
